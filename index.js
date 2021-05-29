7540b30c9c620f91e5b3451478a55cd3d77eb119
Andrew McIndoe <amcindoe@powerhrg.com>
2021-05-29 16:46:03 -0400

    DOM Content Loaded Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() { 
+    let item = document.getElementById("text").innerHTML = "This is really cool!"
+    console.log(item)
+  });
+
+
+// .innerHTML or .textContent +=
